95459634187e1abf10329dde0299356ede018e07
Gautam Mitra <gmitra15@gmail.com>
2018-08-15 13:32:01 +0800

    creating options

--- replace.sh
+++ replace.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+while getopts ":p:k:" opt; do
+  case $opt in
+    p)
+      echo "Piwik URL: $OPTARG" >&2
+      ;;
+    k)
+      echo "Piwik ID Number: $OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option : -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
